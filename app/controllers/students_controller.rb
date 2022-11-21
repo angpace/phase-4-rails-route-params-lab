bc2a9410fa6896facb8dc66eea0431e0d5c3dc05
angpace <108838753+angpace@users.noreply.github.com>
2022-11-21 17:20:52 -0500

    commit tree 03ece82a233fe340871c9255307f3d5ad6a85583

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,16 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+    # The index should return an array of all the students 
+    # If the route is call with a query param, it should return the student that matches
+    students =  Student.all
+    render json: students 
+
+  end
+
+  def show
+    findId = Student.find_by(id: params[:id])
+    render json: findId
   end
 
 end
